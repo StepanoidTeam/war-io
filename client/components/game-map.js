f7bbfddacb4a09a4e1c88093d30a791302dfd411
Volodymyr Myshko <volodymyrmyshko@Administrators-MacBook-Pro.local>
2019-06-29 23:48:37 +0300

    brush chains refac - water/ice/snow work OK

--- client/components/game-map.js
+++ client/components/game-map.js
@@ -4,6 +4,27 @@ import { MapCell } from "./map-cell.js";
 import { winterTileSet } from "../tilesets.js";
 import { getRandomItem } from "../helpers/random.js";
 
+// brush/layer dependency order
+const brushChains = {
+  [editor.brushes.water]: [
+    editor.brushes.snow,
+    editor.brushes.ice,
+    editor.brushes.water,
+  ],
+  [editor.brushes.snow]: [editor.brushes.ice, editor.brushes.snow],
+  [editor.brushes.forest]: [
+    editor.brushes.ice,
+    editor.brushes.snow,
+    editor.brushes.forest,
+  ],
+  // [editor.brushes.rocks]: [editor.brushes.ice],
+  [editor.brushes.ice]: [
+    // editor.brushes.water, //2
+    // editor.brushes.snow, //these two are same prior, how to handle that?
+    editor.brushes.ice,
+  ],
+};
+
 export class GameMap {
   constructor({ size }) {
     this.size = size;
@@ -58,25 +79,25 @@ export class GameMap {
     return this.cells[index];
   }
 
-  eachSibling(col, row, brushSize = 1, callback) {
+  eachCellInArea(col, row, areaSize = 1, callback) {
     //neighbour cells
-    for (let rowShift = -brushSize; rowShift <= brushSize; rowShift++) {
-      for (let colShift = -brushSize; colShift <= brushSize; colShift++) {
+    for (let rowShift = -areaSize; rowShift <= areaSize; rowShift++) {
+      for (let colShift = -areaSize; colShift <= areaSize; colShift++) {
         const cell = this.getCell(col + colShift, row + rowShift);
         if (!cell) continue;
 
-        callback(cell, colShift, rowShift, brushSize);
+        callback(cell, colShift, rowShift, areaSize);
       }
     }
   }
   //todo: concat with above?
-  brushSiblings(
+  paintCell(
     cell,
     sibShiftCol = 0,
     sibShiftRow = 0,
-    brushSize = 0,
+    areaSize = 0,
     currentBrush,
-    baseBrushes = []
+    brushesToSkip = []
   ) {
     const code = cell.tileCode.split("");
 
@@ -84,14 +105,14 @@ export class GameMap {
       for (let tileCol = 0; tileCol <= 1; tileCol++) {
         //todo: how to simplify these conditions?
         //bokovushki
-        if (sibShiftCol === -brushSize && tileCol === 0) continue;
-        if (sibShiftCol === +brushSize && tileCol === 1) continue; //sx=2(t),1
-        if (sibShiftRow === -brushSize && tileRow === 0) continue;
-        if (sibShiftRow === +brushSize && tileRow === 1) continue;
+        if (sibShiftCol === -areaSize && tileCol === 0) continue;
+        if (sibShiftCol === +areaSize && tileCol === 1) continue; //sx=2(t),1
+        if (sibShiftRow === -areaSize && tileRow === 0) continue;
+        if (sibShiftRow === +areaSize && tileRow === 1) continue;
 
         const index = tileCol + tileRow * 2;
         //do not overlap basic brush
-        if (baseBrushes.includes(code[index])) continue;
+        if (brushesToSkip.includes(code[index])) continue;
 
         code[index] = currentBrush;
       }
@@ -111,33 +132,15 @@ export class GameMap {
     const col = Math.floor(layerX / cellSizePx);
     const row = Math.floor(layerY / cellSizePx);
 
-    const deps = {
-      [editor.brushes.water]: editor.brushes.ice,
-      [editor.brushes.snow]: editor.brushes.ice, // editor.brushes.forest],
-      [editor.brushes.forest]: editor.brushes.snow,
-      // [editor.brushes.rocks]: [editor.brushes.ice],
-      // [editor.brushes.ice]: [
-      //   editor.brushes.snow,
-      //   editor.brushes.water,
-      //   editor.brushes.rocks,
-      // ],
-    };
-    //todo: use better naming
-    const depBrush = deps[editor.brush];
-    if (depBrush) {
-      this.eachSibling(col, row, editor.brushSize + 1, (...args) =>
-        this.brushSiblings(...args, depBrush, [editor.brush, depBrush])
+    const brushChain = brushChains[editor.brush];
+
+    brushChain.forEach((brush, index, { length }) => {
+      const size = length - index - 1 + editor.brushSize;
+
+      this.eachCellInArea(col, row, size, (...args) =>
+        this.paintCell(...args, brush, brushChain.slice(index))
       );
-    }
-
-    //setTimeout(() => {
-    //todo: apply selected brush
-    this.eachSibling(col, row, editor.brushSize, (...args) =>
-      this.brushSiblings(...args, editor.brush)
-    );
-    // }, 1000);
-
-    //select cell
+    });
 
     //console.log(col, row, index);
   };

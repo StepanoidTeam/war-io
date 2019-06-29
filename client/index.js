0e28c30ee26a4aa0ef3850ac0e24c9222fe019df
Volodymyr Myshko <volodymyrmyshko@Administrators-MacBook-Pro.local>
2019-06-29 14:55:16 +0300

    canvas clean iml

--- client/index.js
+++ client/index.js
@@ -1,11 +1,18 @@
 import { ctx } from "./context.js";
-
 import { GameMap } from "./components/game-map.js";
-import { Tile } from "./components/tile.js";
-import { debug } from "./config.js";
+import { debug, brushes } from "./config.js";
+
+const canvasCleaner = {
+  draw(ctx) {
+    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+  },
+};
 
 (async () => {
-  const drawables = [new GameMap({ size: { rows: 20, cols: 20 } })];
+  const drawables = [
+    canvasCleaner,
+    new GameMap({ size: { rows: 20, cols: 20 } }),
+  ];
 
   requestAnimationFrame(function render() {
     drawables.forEach(s => s.draw(ctx));
@@ -13,8 +20,8 @@ import { debug } from "./config.js";
   });
 })();
 
+//dat-gui
 const gui = new dat.GUI();
-
-gui.add(debug, "brush", { snow: 1, ice: 0 });
+gui.add(debug, "brush", brushes);
 gui.add(debug, "enabled");
 gui.addColor(debug, "cellColor");

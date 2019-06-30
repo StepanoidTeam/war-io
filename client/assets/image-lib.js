6e2693323bf3c256369f3650b8fde3bb65a530bd
Volodymyr Myshko <volodymyrmyshko@Administrators-MacBook-Pro.local>
2019-06-30 18:21:47 +0300

    image lib extractions

---
+++ client/assets/image-lib.js
@@ -0,0 +1,8 @@
+export const IMAGES = {
+  DEBUG: "debug",
+  CURSOR: "cursor",
+  CELL: "cell",
+  BRUSH_MARKERS: "brush_markers",
+};
+
+export const imageLib = new Map();
---
+++ client/assets/index.js
@@ -0,0 +1,5 @@
+export { imageLib, IMAGES } from "./image-lib.js";
+
+//todo: too complex dependencies - simplify
+import "./brush-markers.js";
+import "./rect-images.js";

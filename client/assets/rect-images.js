32d5dc3d1fa34d8d05d004ff849cf7fb6624d78f
Volodymyr Myshko <volodymyrmyshko@Administrators-MacBook-Pro.local>
2019-06-30 18:19:30 +0300

    rects generate as images

---
+++ client/assets/rect-images.js
@@ -0,0 +1,22 @@
+import { IMAGES, imageLib } from "./image-lib.js";
+import { cellSizePx, editor } from "../config.js";
+
+function createRectImage({ color, size = cellSizePx }) {
+  //const ctx = new OffscreenCanvas(size, size).getContext("2d");
+  const ctx = document.createElement("canvas").getContext("2d");
+  //debug
+  //document.body.append(ctx.canvas);
+
+  ctx.canvas.width = size;
+  ctx.canvas.height = size;
+
+  ctx.strokeStyle = color;
+  ctx.strokeRect(0, 0, size, size);
+
+  return ctx.canvas;
+}
+
+//generate rects
+imageLib.set(IMAGES.DEBUG, createRectImage({ color: "red" }));
+imageLib.set(IMAGES.CURSOR, createRectImage({ color: editor.cursorColor }));
+imageLib.set(IMAGES.CELL, createRectImage({ color: "white" }));

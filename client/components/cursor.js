25561d702da480e52496c85bc6a1771143afb4fa
Volodymyr Myshko <volodymyrmyshko@Administrators-MacBook-Pro.local>
2019-06-30 18:21:00 +0300

    cursor draws from tile

--- client/components/cursor.js
+++ client/components/cursor.js
@@ -1,15 +1,17 @@
-import { editor, cellSizePx } from "../config.js";
+import { cellSizePx } from "../config.js";
+import { cursorTile } from "./tile.js";
 
 export const cursor = {
   x: 0,
   y: 0,
+  tile: cursorTile,
   move(x, y) {
     this.x = x;
     this.y = y;
   },
   draw(ctx) {
-    ctx.strokeStyle = editor.cursorColor;
-    ctx.strokeRect(
+    ctx.drawImage(
+      ...this.tile,
       this.x * cellSizePx,
       this.y * cellSizePx,
       cellSizePx,

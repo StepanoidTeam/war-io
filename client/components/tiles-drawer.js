ff26a9598df7c3058bb09d86406afe9ac6e7d34f
bobanko <mishko.vladimir@gmail.com>
2019-07-03 01:55:10 +0300

    tiles-drawer added to build clean tileset - in progress

---
+++ client/components/tiles-drawer.js
@@ -0,0 +1,28 @@
+import { winterTileSet } from "../tilesets.js";
+import { editor } from "../config.js";
+
+//debug: to draw all used tiles from tileset
+export const tilesDrawer = {
+  tiles: Object.entries(winterTileSet),
+  draw(ctx) {
+    let x = 0,
+      y = 0;
+    this.tiles.forEach(([, tileGroup], groupIndex) => {
+      Object.values(tileGroup).forEach((tile, index) => {
+        ctx.drawImage(
+          ...tile,
+          x * editor.cellSizePx,
+          y * editor.cellSizePx,
+          editor.cellSizePx,
+          editor.cellSizePx
+        );
+
+        x++;
+        if (x >= 20) {
+          x = 0;
+          y++;
+        }
+      });
+    });
+  },
+};

45a73e6f3b9ed4947c1528358474c57241d2292a
bobanko <mishko.vladimir@gmail.com>
2019-07-07 01:52:21 +0300

    common/index removed as redundant

--- client/common/index.js
+++
@@ -1,6 +0,0 @@
-export { imageLib, IMAGES } from "./image-lib.js";
-export { brushChains } from "./brush-chains.js";
-
-//todo: too complex dependencies - simplify
-import "./brush-markers.js";
-import "./rect-images.js";
